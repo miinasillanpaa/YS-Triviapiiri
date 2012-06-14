6b5748d55d2a006a28c0cf87eac43f9e81eee9f2
Teemu Huttunen <teemu@huttunen.fi>
2012-06-14 15:38:30 +0300

    selectGameView added, models added

--- js/app.js
+++ js/app.js
@@ -1,29 +1,70 @@
 var Trivia = Em.Application.create();
 
-Trivia.MyView = Em.View.extend({
-  mouseDown: function() {
-    window.alert("hello world!");
-  }
+Trivia.Game = Em.Object.extend({
+    guid: null,
+    name: null
 });
 
-
 Trivia.Question = Em.Object.extend({
+    guid: null,
+    gameId: null,
+    mediaId: null,
+    options: [],
 	questionText: null,
 	answers: [],
 	correctAnswer: null
 });
 
+Trivia.Media = Em.Object.extend({
+    guid: null,
+    media: null,
+    mediaType: null
+});
+
 Trivia.Answer = Em.Object.extend({
+    guid: null,
+    questionId: null,
 	answerText: null,
 	answered: false,
 	correct: false
 });
 
+Trivia.games = [
+    Trivia.Game.create({
+        guid: 1,
+        name: 'Visailu 1'
+    }),
+    Trivia.Game.create({
+        guid: 2,
+        name: 'Yhteinen Sävel - Kulkurin Valssi I'
+    })
+];
 
+soundManager.onready(function() {
+    Trivia.medias = [
+        Trivia.Media.create({
+            guid: 1,
+            mediaType: 'mp3',
+            media: soundManager.createSound({
+                    id: 'kulkurin valssi',
+                    url: '/triviapiiri/assets/Kulkurinvalssi.mp3',
+                    whileplaying: function() {
+                        /*
+                        if (mySound.position >= intervals[nextStopInterval]) {
+                            mySound.stop();
+                            nextStopInterval++;
+                            $('#status').html('<br/>Soitto lopetettu kohtaan ' + mySound.position + 'ms <a href="javascript: void(0);" onclick="continuePlay('+mySound.position+')">Jatka seuraavaan kohtaan</a>');
+                        }*/
+                    }
+            })
+        })
+    ];
+});
 
 Trivia.questions = [
 	Trivia.Question.create({
 		questionText: 'Vaaleaorakas on?',
+        gameId: 1,
 		answers: [
 			Trivia.Answer.create({ answerText: 'Kissarotu' }),
 			Trivia.Answer.create({ answerText: 'Kukka' }),
@@ -33,6 +74,7 @@ Trivia.questions = [
 	}),
 	Trivia.Question.create({
 		questionText: 'Alaston ase -elokuvien pääroolia näytteli?',
+        gameId: 1,
 		answers: [
 			Trivia.Answer.create({ answerText: 'Leslie Nielsen', correct: true  }),
 			Trivia.Answer.create({ answerText: 'Sean Connery' }),
@@ -42,6 +84,7 @@ Trivia.questions = [
 	}),
 	Trivia.Question.create({
 		questionText: 'Lapatossu-elokuvassa rakennetaan?',
+        gameId: 1,
 		answers: [
 			Trivia.Answer.create({ answerText: 'Kirkkoa' }),
 			Trivia.Answer.create({ answerText: 'Omakotitaloa' }),
@@ -51,6 +94,7 @@ Trivia.questions = [
 	}),
 	Trivia.Question.create({
 		questionText: 'Minkä automerkin automalli on Xantia?',
+        gameId: 1,
 		answers: [
 			Trivia.Answer.create({ answerText: 'Audin' }),
 			Trivia.Answer.create({ answerText: 'BMW:n' }),
@@ -60,6 +104,7 @@ Trivia.questions = [
 	}),
 	Trivia.Question.create({
 		questionText: 'Jive on?',
+        gameId: 1,
 		answers: [
 			Trivia.Answer.create({ answerText: 'Lyhytelokuva' }),
 			Trivia.Answer.create({ answerText: 'Satukertomus' }),
@@ -69,6 +114,7 @@ Trivia.questions = [
 	}),
 	Trivia.Question.create({
 		questionText: 'Ars Fennica on?',
+        gameId: 1,
 		answers: [
 			Trivia.Answer.create({ answerText: 'Ilmastovyöhyke' }),
 			Trivia.Answer.create({ answerText: 'Jäänmurtaja' }),
@@ -78,6 +124,7 @@ Trivia.questions = [
 	}),
 	Trivia.Question.create({
 		questionText: 'Vimpa on?',
+        gameId: 1,
 		answers: [
 			Trivia.Answer.create({ answerText: 'Kalanpyydys' }),
 			Trivia.Answer.create({ answerText: 'Kutupaikka' }),
@@ -87,6 +134,7 @@ Trivia.questions = [
 	}),
 	Trivia.Question.create({
 		questionText: 'John Wayne tunnetaan erityisesti?',
+        gameId: 1,
 		answers: [
 			Trivia.Answer.create({ answerText: 'Dokumenttielokuvista' }),
 			Trivia.Answer.create({ answerText: 'Komediaelokuvista' }),
@@ -96,6 +144,7 @@ Trivia.questions = [
 	}),
 	Trivia.Question.create({
 		questionText: 'Akvamariini on?',
+        gameId: 1,
 		answers: [
 			Trivia.Answer.create({ answerText: 'Akvaariokala' }),
 			Trivia.Answer.create({ answerText: 'Jalokivi', correct: true }),
@@ -105,17 +154,111 @@ Trivia.questions = [
 	}),
 	Trivia.Question.create({
 		questionText: 'Ruokasuola on pääasiassa?',
+        gameId: 1,
 		answers: [
 			Trivia.Answer.create({ answerText: 'Kalsiumia' }),
 			Trivia.Answer.create({ answerText: 'Natriumkloridia', correct: true }),
 			Trivia.Answer.create({ answerText: 'Otsonia'}),
 			Trivia.Answer.create({ answerText: 'Typpioksidia' })
 		]
-	})
+	}),
+
+    Trivia.Question.create({
+        gameId: 2,
+        mediaId: 1,
+        questionText: 'Kuuntele ote kappaleesta ja arvaa miten sanat jatkuvat',
+        options: {playTo: 15750},
+        answers: [
+            Trivia.Answer.create({ answerText: 'on morsiamell\' kruunattu pää', correct: true }),
+            Trivia.Answer.create({ answerText: 'on morsian ja sulhanen tääl'}),
+            Trivia.Answer.create({ answerText: 'morsiamell\' on palmikoitu pää'})
+        ]
+    }),
+    Trivia.Question.create({
+        gameId: 2,
+        mediaId: 1,
+        questionText: 'Kuuntele ote kappaleesta ja arvaa miten sanat jatkuvat',
+        options: {playTo: 25800},
+        answers: [
+            Trivia.Answer.create({ answerText: 'miten hauskaa voi olla tää vaan' }),
+            Trivia.Answer.create({ answerText: 'sydän kylmä voi olla kuin jää', correct: true }),
+            Trivia.Answer.create({ answerText: 'kylmään sydämmeen lämpöä tuo'})
+        ]
+    }),
+    Trivia.Question.create({
+        gameId: 2,
+        mediaId: 1,
+        questionText: 'Kuuntele ote kappaleesta ja arvaa miten sanat jatkuvat',
+        options: {playTo: 56000},
+        answers: [
+            Trivia.Answer.create({ answerText: 'ja loistettaan välkehtii' }),
+            Trivia.Answer.create({ answerText: 'lasit kristallin välkehtii' }),
+            Trivia.Answer.create({ answerText: 'ja kristallit kimaltelee', correct: true})
+        ]
+    }),
+    Trivia.Question.create({
+        gameId: 2,
+        mediaId: 1,
+        questionText: 'Kuuntele ote kappaleesta ja arvaa miten sanat jatkuvat',
+        options: {playTo: 112900},
+        answers: [
+            Trivia.Answer.create({ answerText: 'raitilla valssiks' }),
+            Trivia.Answer.create({ answerText: 'raitilla tanssiks', correct: true }),
+            Trivia.Answer.create({ answerText: 'maantiellä juoksuks' })
+        ]
+    }),
+    Trivia.Question.create({
+        gameId: 2,
+        mediaId: 1,
+        questionText: 'Kuuntele ote kappaleesta ja arvaa miten sanat jatkuvat',
+        options: {playTo: 132900},
+        answers: [
+            Trivia.Answer.create({ answerText: 'rakastan ja kaihoan ain\'', correct: true }),
+            Trivia.Answer.create({ answerText: 'minä aina ikävöin vain' }),
+            Trivia.Answer.create({ answerText: 'koskaan mä unhoita en' })
+        ]
+    }),
+    Trivia.Question.create({
+        gameId: 2,
+        mediaId: 1,
+        questionText: 'Kuuntele ote kappaleesta ja arvaa miten sanat jatkuvat',
+        options: {playTo: 146700},
+        answers: [
+            Trivia.Answer.create({ answerText: 'unelmat repussaan' }),
+            Trivia.Answer.create({ answerText: 'kantaen kohtaloaan', correct: true }),
+            Trivia.Answer.create({ answerText: 'odottaen tulevaa' })
+        ]
+    }),
+    Trivia.Question.create({
+        gameId: 2,
+        mediaId: 1,
+        questionText: 'Kuuntele ote kappaleesta ja arvaa miten sanat jatkuvat',
+        options: {playTo: 224600},
+        answers: [
+            Trivia.Answer.create({ answerText: 'kiristää vyö', correct: true }),
+            Trivia.Answer.create({ answerText: 'ahdistaa työ'}),
+            Trivia.Answer.create({ answerText: 'valssiksi lyön' })
+        ]
+    })
 ];
 
-Trivia.AppView = Em.View.extend({
-	templateName: 'app',
+Trivia.selectGameView = Em.CollectionView.extend({
+    templateName: 'selectGame',
+    tagName: 'ul',
+    contentBinding: 'Trivia.games',
+    itemViewClass: Em.View.extend({
+        tagName: 'li',
+
+        classNames: 'answer-view btn',
+        click: function() {
+
+        }
+    })
+
+});
+
+Trivia.GameView = Em.View.extend({
+	templateName: 'game',
 	scoreBinding: 'Trivia.gameController.score',
 	questionBinding: 'Trivia.gameController.question',
 	questionView: Em.View.extend({
